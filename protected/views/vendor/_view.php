96561521d1b0555128617e51948fdec192b40273
Joseph Gaston <joseph.gaston@yahoo.com>
2014-09-23 14:20:58 -0500

    Added vendor model + cruddy views + vendor controller

---
+++ protected/views/vendor/_view.php
@@ -0,0 +1,21 @@
+<?php
+/* @var $this VendorController */
+/* @var $data Vendor */
+?>
+
+<div class="view">
+
+    <b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+    <?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+    <br />
+
+    <b><?php echo CHtml::encode($data->getAttributeLabel('name')); ?>:</b>
+    <?php echo CHtml::encode($data->name); ?>
+    <br />
+
+    <b><?php echo CHtml::encode($data->getAttributeLabel('domain')); ?>:</b>
+    <?php echo CHtml::encode($data->domain); ?>
+    <br />
+
+
+</div>
