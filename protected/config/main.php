468be133e1da247a042356b543ac97821f9997c9
Brittany Nelms <brittany_nelms@yahoo.com>
2014-10-28 13:23:28 -0500

    Installed and configured new theme

--- protected/config/main.php
+++ protected/config/main.php
@@ -9,6 +9,8 @@ return array(
     'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
     'name'=>'Media Library Price Tracker',
 
+    'theme' => 'biskit',
+
     // preloading 'log' component
     'preload'=>array('log'),
 
